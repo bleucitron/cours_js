853d63388ae76a5d51acefb11f3a12decfd5822d
Romain Crestey <rcrestey@gmail.com>
2021-10-14 10:26:12 +0200

    2021-10-14

--- exos/3_browser/index.js
+++ exos/3_browser/index.js
@@ -41,8 +41,20 @@ colors.forEach(function (color, position) {
     - Créer une <section> avec l'id 'exo2', et l'ajouter au body
     - Créer une <div> carrée, de couleur noire, et l'ajouter à la 2e section
     - Lui ajouter un listener au mousemove, qui change sa largeur
-    en fonction de la position en Y de la souris à l'écran (event.clientY)
+    en fonction de la position de la souris à l'écran (event.clientX)
 */
+
+const exo2 = document.createElement('section');
+const carre = document.createElement('div');
+document.body.append(exo2);
+exo2.append(carre);
+carre.classList.add('carre');
+
+carre.addEventListener('mousemove', function (event) {
+  carre.style.width = event.clientX + 'px';
+});
+
+console.log(localStorage.getItem('nb'));
 
 // -------------------------------
 
@@ -56,6 +68,42 @@ colors.forEach(function (color, position) {
     pour recharger la page avec le bon nombre de points dès le début
 */
 
+const fromStorage = localStorage.getItem('counter');
+
+let counter = fromStorage ? parseInt(fromStorage) : 0;
+
+const exo3 = document.createElement('section');
+const nb = document.createElement('div');
+const dots = document.createElement('div');
+document.body.append(exo3);
+exo3.append(nb);
+exo3.append(dots);
+nb.classList.add('nb');
+dots.classList.add('dots');
+
+nb.textContent = counter;
+
+function addDot() {
+  const dot = document.createElement('div');
+  dot.classList.add('dot');
+  dots.append(dot);
+}
+
+for (let i = 0; i < counter; i++) {
+  addDot();
+}
+
+function startCounting() {
+  return setInterval(function () {
+    addDot();
+    counter += 1;
+    localStorage.setItem('counter', counter);
+
+    nb.textContent = counter;
+  }, 1000);
+}
+
+let interval = startCounting();
 // -------------------------------
 
 /* Exercice 4: Contrôle au clavier
@@ -66,11 +114,37 @@ colors.forEach(function (color, position) {
     - Faire en sorte de d'arrêter le timer quand on appuie sur S, et de le relancer en réappuyant
 */
 
+document.addEventListener('keydown', function (event) {
+  const key = event.key;
+  const isCtrl = event.metaKey;
+
+  if (key >= 1 && key < colors.length) {
+    document.body.style.background = colors[event.key - 1];
+  } else if (isCtrl && key === 'r') {
+    event.preventDefault();
+    exo1.remove();
+    exo2.remove();
+    exo3.remove();
+    exoBonus.remove();
+  } else if (key === 'Enter') {
+    document.body.append(exo1);
+    document.body.append(exo2);
+    document.body.append(exo3);
+    document.body.append(exoBonus);
+  } else if (key === 's') {
+    if (interval) {
+      interval = clearInterval(interval);
+    } else {
+      interval = startCounting();
+    }
+  }
+});
+
 // -------------------------------
 
 /* Exercice BONUS: Harry Potter
     - Créer une <section> avec l'id 'exoBonus', et l'ajouter au body
-    - Créer une <div> pour Harry, avec la classe 'character' et l'ajouter à la 3e section
+    - Créer une <div> pour Harry, avec la classe 'character' et l'ajouter à 'exoBonus'
     - La div 'character' a pour enfant une div avec la classe 'name' avec le nom en textContent
     et une img avec le src correspondant
     - Ajouter un listener qui, au click, choisit un personnage au hasard
@@ -105,3 +179,46 @@ const characters = [
     src: 'static/Dumbledore_and_Elder_Wand.jpeg',
   },
 ];
+
+const exoBonus = document.createElement('section');
+exoBonus.id = 'exoBonus';
+document.body.append(exoBonus);
+
+function createCharacter(person) {
+  const character = document.createElement('div');
+  character.classList.add('character');
+
+  const name = document.createElement('div');
+  name.classList.add('name');
+  name.textContent = person.name;
+  const img = document.createElement('img');
+  img.classList.add('img');
+  img.src = person.src;
+  img.alt = 'Picture of ' + person.name;
+
+  character.append(name);
+  character.append(img);
+
+  character.addEventListener('click', function () {
+    // remove current character to avoid picking the same one
+    const allCharactersButCurrent = characters.filter(function (c) {
+      return c.name !== person.name;
+    });
+
+    // choose random position
+    const randomPosition = Math.floor(
+      Math.random() * allCharactersButCurrent.length,
+    );
+    const newCharacter = allCharactersButCurrent[randomPosition];
+
+    // recreate new div with recursivity
+    const newDiv = createCharacter(newCharacter);
+    // replace current with new
+    character.replaceWith(newDiv);
+  });
+
+  return character;
+}
+
+const current = createCharacter(characters[0]);
+exoBonus.append(current);
