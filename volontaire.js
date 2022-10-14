dc0ec9a70cd8345736aaea6b938725bcdb9af613
Romain Crestey <rcrestey@gmail.com>
2022-10-14 11:04:10 +0200

    removed volontaire.js

--- volontaire.js
+++
@@ -1,8 +0,0 @@
-const allNames = ['Alexis', 'Mathilde', 'Gaëtan', 'Corentin', 'Antoine'];
-
-let names = allNames;
-
-const n = Math.floor(Math.random() * allNames.length);
-
-const volontaire = allNames[n];
-console.log('Next', volontaire);
