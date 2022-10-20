df7514a95a1085da605fbc701f6bf39389b9b801
Romain Crestey <rcrestey@gmail.com>
2022-10-20 17:52:47 +0200

    2022-10-20

--- exos/3_browser/3-2_dom/index.js
+++ exos/3_browser/3-2_dom/index.js
@@ -23,6 +23,51 @@ console.log('*** Chirurgie ***');
  * - Ajoutez en textContent de ces éléments leur numero (entre 1 et 15) et ajoutez les à la 2e section
  */
 
+const idCoucou = document.getElementById('coucou');
+idCoucou.style.background = 'green';
+idCoucou.textContent = 'Hey !';
+
+const allI = document.querySelectorAll('i');
+const allRed = document.querySelectorAll('i.blue');
+const allBlue = document.querySelectorAll('i.red');
+
+const nbAll = allI.length;
+const nbBlue = allBlue.length;
+const nbRed = allRed.length;
+
+console.log(nbAll, nbBlue, nbRed, nbAll - nbBlue - nbRed);
+
+allRed.forEach(e => e.remove());
+allBlue.forEach(e => e.remove());
+document.querySelectorAll('.inutile').forEach(e => e.remove());
+
+const footer = document.querySelector('footer');
+const p = document.createElement('p');
+p.textContent = 'Bègles';
+p.classList.add('blue');
+footer.append(p);
+
+const h3 = document.createElement('h3');
+h3.textContent = 'Infos';
+h3.classList.add('red');
+footer.prepend(h3);
+
+const section2 = document.querySelectorAll('section')[1];
+
+for (let i = 1; i <= 15; i++) {
+  const div = document.createElement('div');
+  div.classList.add('numero');
+  div.textContent = i;
+  section2.append(div);
+}
+
+// Array.from({ length: 15 }).map((n, i) => {
+//   const div = document.createElement('div');
+//   div.classList.add('numero');
+//   div.textContent = i + 1;
+//   section2.append(div);
+// });
+
 /**========================================================================
  *                           [BONUS] Article
  *========================================================================**/
